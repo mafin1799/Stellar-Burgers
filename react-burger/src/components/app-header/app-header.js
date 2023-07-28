621d7577a98076be4bb0ba570b67e9d27cd97bc5
chuiko <chuiko@bolid.ru>
2023-07-28 18:10:55 +0300

    fix

--- react-burger/src/components/app-header/app-header.js
+++ react-burger/src/components/app-header/app-header.js
@@ -1,12 +1,10 @@
-import React from "react";
-
 import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
-import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
-import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
+import { BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
+
 import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/fonts/fonts.css";
 import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css"
-import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
 import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css"
+
 import styles from "../../assets/app-header.module.css"
 
 const NavMenu = ({ children }) => {
@@ -16,53 +14,54 @@ const NavMenu = ({ children }) => {
         </div>
     )
 }
+
 const NavLink = ({ path, text, pressed, children }) => {
     const text_style = pressed ? "text_color_primary" : "text_color_inactive";
     return (
-        <a href={path} className="text_type_main-default text_color_primary">
-            <div className={`${styles.container} pt-4 pb-4 pl-5 pr-5`}>
-                <i className="pr-2">{children}</i>
-                <span className={`text ${text_style}`}>{text}</span>
-            </div>
-        </a>
+        <div className="col-md-auto">
+            <a href={path} className="text_type_main-default text_color_primary">
+                <div className={`${styles.container} pt-4 pb-4 pl-5 pr-5`}>
+                    <i className="pr-2">{children}</i>
+                    <span className={`text ${text_style}`}>{text}</span>
+                </div>
+            </a>
+        </div>
     )
 }
 
+const Navigate = ({ children }) => {
+    return (
+        <div className="col d-flex align-items-center justify-content-start">
+            <div className="row p-0 m-0">
+                {children}
+            </div>
+        </div>
+    )
 
+}
 
 function AppHeader() {
     return (
         <NavMenu>
-            <div className={`col d-flex align-items-center justify-content-start`}>
-                <div className="row p-0 m-0">
-                    <div className={`col-md-auto`}>
-                        <NavLink path="#" text="Конструктор" pressed={true}>
-                            <BurgerIcon type="primary" />
-                        </NavLink>
-                    </div>
-                    <div className={`col-md-auto`}>
-                        <NavLink path="#" text="Лента заказов" pressed={false}>
-                            <ListIcon type="secondary" />
-                        </NavLink>
-                    </div>
-                </div>
-            </div>
+            <Navigate>
+                <NavLink path="#" text="Конструктор" pressed={true}>
+                    <BurgerIcon type="primary" />
+                </NavLink>
+                <NavLink path="#" text="Лента заказов" pressed={false}>
+                    <ListIcon type="secondary" />
+                </NavLink>
+            </Navigate>
 
-            <div className={`col d-flex align-items-center justify-content-center`}>
+            <div className="col d-flex align-items-center justify-content-center">
                 <Logo />
             </div>
 
-            <div className={`col d-flex justify-content-end`}>
+            <div className="col d-flex justify-content-end">
                 <NavLink path="#" text="Личный кабинет" pressed={false} >
                     <ProfileIcon type="secondary" />
                 </NavLink>
             </div>
-
-
-
         </NavMenu>
-
-
     )
 }
 
