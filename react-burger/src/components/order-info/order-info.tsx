b6397f7282edf711fb32d254b0f4a047db29d5f6
chuiko <chuiko@bolid.ru>
2023-10-04 14:45:26 +0300

    fix

--- react-burger/src/components/order-info/order-info.tsx
+++ react-burger/src/components/order-info/order-info.tsx
@@ -15,8 +15,6 @@ type TOrderInfo = {
 export const OrderInfo: FC<TOrderInfo> = ({ modal, data }) => {
   const { id } = useParams();
   const navigate = useNavigate();
-
-  console.log(data)
   const current = data!.find(item => item._id === id);
   const ingredientsInfo = useSelector((store) => store.ingredientsInfo.ingredients);
   const orderingredients = current?.ingredients.map(item => ingredientsInfo!.find(data => data._id === item));
