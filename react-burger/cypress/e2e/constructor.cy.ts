99456cc58c7be875e3f52717faa4c2f8bed8d364
chuiko <chuiko@bolid.ru>
2023-10-12 11:40:51 +0300

    ff

--- react-burger/cypress/e2e/constructor.cy.ts
+++ react-burger/cypress/e2e/constructor.cy.ts
@@ -1,27 +1,24 @@
 import { STELLAR_BERGER_API } from '../../src/utils/const';
 
-describe("Constructor page works correctly", () => {
+describe("Страничка конструктора", () => {
     beforeEach(() => {
         cy.intercept("POST", `${STELLAR_BERGER_API}/orders`).as("postOrder");
         cy.intercept("POST", `${STELLAR_BERGER_API}/auth/login`).as("login");
-
         cy.intercept("GET", `${STELLAR_BERGER_API}/ingredients`).as("getIngredients");
-
         cy.visit("http://localhost:3000/login");
         cy.get("[name^=email]").type("nik.chujko@mail.ru");
         cy.get("[name^=password]").type("952410");
         cy.get("button").contains("Войти").click();
         cy.wait("@login");
-
     });
 
-    it("should open ingredients popup", () => {
+    it("открытие попапа с ингридиентом", () => {
         cy.wait("@getIngredients");
         cy.contains("Краторная булка N-200i").click();
         cy.get("button").click();
     });
 
-    it("constructor should work", () => {
+    it("работа оформления заказа", () => {
         cy.visit("http://localhost:3000");
         cy.viewport(1920, 1080) // Set viewport to 550px x 750px
         cy.contains("Мясо").as("main");
